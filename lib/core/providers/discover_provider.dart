a8fb63a81274983e56fe4aae29855355d7e88f36
Diego Balestra <diegobalestra9@gmail.com>
2023-03-25 22:31:25 -0500

    Read and write to provider

--- lib/core/providers/discover_provider.dart
+++ lib/core/providers/discover_provider.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 import '/domain/entities/video_post.dart';
+import '/infraestructure/models/local_video_model.dart';
+import '/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future loadNextPage() async {
-    // TODO cargar videos
+    await Future.delayed(const Duration(seconds: 2));
+
+    List<VideoPost> newVideos = videoPosts.map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
 
     notifyListeners();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => DiscoverProvider()),
+        ChangeNotifierProvider(
+          lazy: false,
+          create: (_) => DiscoverProvider()..loadNextPage(),
+        ),
       ],
       child: MaterialApp(
         title: 'TokTik',
