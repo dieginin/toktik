0c48d762f8588f6322bb29535dca2ec3dead6bc9
Diego Balestra <diegobalestra9@gmail.com>
2023-03-26 11:59:06 -0500

    Added spinning video button

--- lib/core/widgets/shared/video_buttons.dart
+++ lib/core/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 import '/config/helpers/human_format.dart';
@@ -26,7 +27,7 @@ class _CustomIconButton extends StatelessWidget {
             size: 30,
           ),
         ),
-        Text(HumanFormat.humanReadbleNumber(value)),
+        if (value > 0) Text(HumanFormat.humanReadbleNumber(value)),
       ],
     );
   }
@@ -49,9 +50,19 @@ class VideoButtons extends StatelessWidget {
           iconData: Icons.favorite,
           iconColor: Colors.red,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 3),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
         )
       ],
     );
